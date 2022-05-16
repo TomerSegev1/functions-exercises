5262411bffc991ccd63439ede8cb837fcb591da4
user <tomi.segev@gmail.com>
2022-05-16 13:41:09 +0300

    ex3

--- functions.js
+++ functions.js
@@ -4,6 +4,12 @@
 // }
 
 //exercise2
-const sum = (a, b, c) => {
-        return a+b+c
+// const sum = (a, b, c) => {
+//         return a+b+c
+//     }
+
+//exercise3
+const calculate = (a, b) => {
+        return console.log(a % b)
     }
+    calculate(4,2)
